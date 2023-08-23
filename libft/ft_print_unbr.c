f41239d659e269ea5f4f4efe30c32f761a4b5ce5
silvagomez <silva.camilo@gmail.com>
2023-08-23 10:34:26 +0200

    Update comment

--- libft/ft_print_unbr.c
+++ libft/ft_print_unbr.c
@@ -6,12 +6,15 @@
 /*   By: dsilva-g <dsilva-g@student.42urduli>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/16 09:41:12 by dsilva-g          #+#    #+#             */
-/*   Updated: 2023/08/21 15:42:17 by dsilva-g         ###   ########.fr       */
+/*   Updated: 2023/08/23 10:33:34 by dsilva-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+/*
+ * For fracinette if (unbr >= 0)
+ */
 int	ft_print_unbr(unsigned int unbr)
 {
 	int	unbr_len;
@@ -21,8 +24,7 @@ int	ft_print_unbr(unsigned int unbr)
 	unbr_len = 0;
 	if (unbr >= 10)
 		unbr_len += ft_print_unbr(unbr / 10);
-	//if (unbr >= 0)
-	else
+	if (unbr >= 0)
 		unbr_len += ft_print_char((unbr % 10) + '0');
 	return (unbr_len);
 }
